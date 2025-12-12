c36c0f2a24023e61b8dc06d87cf5257e1e35e3e6
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2025-12-12 08:31:15 +0530

    feat: add tags and prefix to users router

--- app/features/users/urls.py
+++ app/features/users/urls.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.features.users.views import login, register, users
 
 
-router = APIRouter()
+router = APIRouter(tags=["users"], prefix="/api/v1")
 
 router.include_router(login.router, prefix="/auth")
 router.include_router(register.router, prefix="/auth")
