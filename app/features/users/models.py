50ece9f7909a38f16568401aa7014aea0ae02573
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2025-12-17 23:28:14 +0530

    fix: fix bug in user notification relation

--- app/features/users/models.py
+++ app/features/users/models.py
@@ -25,7 +25,7 @@ class User(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
 
-    notifications: Mapped["Notification"] = relationship(back_populates="user", passive_deletes=True)
+    notifications: Mapped[list["Notification"]] = relationship(back_populates="user", passive_deletes=True)
 
     def set_password(self, password: str):
         password_hasher = PasswordHasher()
