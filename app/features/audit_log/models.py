c0ca7e4fbfefb37f6faaecf484b1f91918a09e08
LakshanSaputhanthri <lakshan.sv25@gmail.com>
2025-12-23 11:11:33 +0530

    feat: remove create_audit_log function and add migration for audit_logs table

--- app/features/audit_log/models.py
+++ app/features/audit_log/models.py
@@ -1,45 +1,45 @@
 import enum
 import uuid
-import typing
 from datetime import datetime
 from typing import Any
 
 
-from sqlalchemy import Column, String, DateTime, JSON, ForeignKey, UUID
+from sqlalchemy import String, DateTime, JSON, ForeignKey, UUID
 from sqlalchemy.sql import func
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import Enum
 
 
 from app.config.database import Base
 
-if typing.TYPE_CHECKING:
-    from app.features.users.models import User
-
 
 class ActorType(enum.Enum):
     USER = "USER"
     SYSTEM = "SYSTEM"
+    ANONYMOUS = "ANONYMOUS"
 
 
 class AuditLog(Base):
     __tablename__ = "audit_logs"
 
-    id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+
     actor_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
-    actor_type: Mapped[str] = Column(Enum("ActorType"), nullable=False)
-    action: Mapped[str] = Column(String, nullable=False)
-    resource_type: Mapped[str] = Column(String, nullable=False)
-    resource: Mapped[str] = Column(String, nullable=False)
-    resource_id: Mapped[str] = Column(String, nullable=True)
-    old_value: Mapped[dict[str, Any]] = Column(JSON, nullable=True)
-    new_value: Mapped[dict[str, Any]] = Column(JSON, nullable=True)
-    changed_value: Mapped[dict[str, Any]] = Column(JSON, nullable=True)
-    request_id: Mapped[str] = Column(String, nullable=True)
-    request_ip_address: Mapped[str] = Column(String, nullable=True)
-    request_user_agent: Mapped[str] = Column(String, nullable=True)
-    request_method: Mapped[str] = Column(String, nullable=True)
-    request_url: Mapped[str] = Column(String, nullable=True)
-    created_at: Mapped[datetime] = Column(DateTime(timezone=True), server_default=func.now())
-    modified_at: Mapped[datetime] = Column(DateTime(timezone=True), onupdate=func.now())
-    user: Mapped["User"] = relationship(back_populates="audit_logs")
+    actor_type: Mapped[str] = mapped_column(Enum(ActorType))
+    action: Mapped[str] = mapped_column(String)
+    resource_type: Mapped[str] = mapped_column(String)
+    resource: Mapped[str] = mapped_column(String)
+    resource_id: Mapped[uuid.UUID] = mapped_column(UUID)
+
+    old_value: Mapped[dict[str, Any] | None] = mapped_column(JSON, nullable=True)
+    new_value: Mapped[dict[str, Any] | None] = mapped_column(JSON, nullable=True)
+    changed_value: Mapped[dict[str, Any] | None] = mapped_column(JSON, nullable=True)
+
+    request_id: Mapped[str | None] = mapped_column(String, nullable=True)
+    request_ip_address: Mapped[str | None] = mapped_column(String, nullable=True)
+    request_user_agent: Mapped[str | None] = mapped_column(String, nullable=True)
+    request_method: Mapped[str | None] = mapped_column(String, nullable=True)
+    request_url: Mapped[str | None] = mapped_column(String, nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    modified_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
